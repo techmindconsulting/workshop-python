7b59b72af839634d252207acc75a717fade00469
techmindconsulting <techmind.consulting.sasu@gmail.com>
2022-08-17 22:19:02 +0200

    update tp-1

--- tp-1-guess-number/main.py
+++ tp-1-guess-number/main.py
@@ -9,27 +9,21 @@ nb_trial = 0
 
 
 def ask_number(nb_min, nb_max):
-    while True:
-        try:
-            global nb_trial
-            nb_trial += 1
-            print('Essai n°{} - '.format(nb_trial), end='')
-            user_number = int(
-                input('Saisir un nombre un nombre entre {} et {} : '.format(
-                    nb_min,
-                    nb_max)
-                )
-            )
-        except Exception:
-            print('Merci de saisir un nombre entre {} et {}'.format(
+    try:
+        user_number = int(
+            input('Saisir un nombre un nombre entre {} et {} : '.format(
                 nb_min,
                 nb_max)
             )
-            if nb_trial >= NB_ATTEMPTS:
-                return 0
-        else:
-            if nb_min <= user_number <= nb_max:
-                return user_number
+        )
+    except Exception:
+        print('Merci de saisir un nombre entre {} et {}'.format(
+            nb_min,
+            nb_max)
+        )
+        user_number = 0
+    finally:
+        return user_number
 
 
 nb_random = random.randint(NB_MIN_RANDOM, NB_MAX_RANDOM)
@@ -37,17 +31,19 @@ nb_min_random = NB_MIN_RANDOM
 nb_max_random = NB_MAX_RANDOM
 
 while nb_trial < NB_ATTEMPTS:
+    nb_trial += 1
+    print('Essai n°{} - '.format(nb_trial), end='')
     nb_user_input = ask_number(nb_min_random, nb_max_random)
     if nb_random == nb_user_input:
-        print('Trouvé au bout de {} tentative(s)'.format(nb_trial))
+        print('GAGNE :D! Trouvé au bout de {} tentative(s)'.format(nb_trial))
         break
     elif nb_user_input > nb_random:
-        nb_max_random = nb_user_input
+        nb_max_random = nb_user_input if nb_user_input <= nb_max_random else nb_max_random
         print('{} est trop grand'.format(nb_user_input))
     else:
         print('{} est trop petit'.format(nb_user_input))
-        nb_min_random = nb_user_input
+        nb_min_random = nb_user_input if nb_user_input != 0 else 1
 
     if nb_trial >= NB_ATTEMPTS:
-        print('Perdu, Le nombre aléatoire était {}'.format(nb_random))
+        print('PERDU :(! Le nombre aléatoire était {}'.format(nb_random))
         break
