231567235a26a08e692f8c57947192896f594638
techmindconsulting <techmind.consulting.sasu@gmail.com>
2022-08-17 20:32:43 +0200

    add use case tp-1

--- tp-1-guess-number/main.py
+++ tp-1-guess-number/main.py
@@ -1,35 +1,48 @@
 import random
 
 
-MIN = 1
-MAX = 100
+NB_MIN_RANDOM = 1
+NB_MAX_RANDOM = 100
 NB_ATTEMPTS = 5
 
 
 def ask_number(nb_min, nb_max):
     while True:
         try:
-            user_number = int(input('Saisir un nombre un nombre entre {} et {} : '.format(MIN, MAX)))
+            user_number = int(
+                input('Saisir un nombre un nombre entre {} et {} : '.format(
+                    nb_min,
+                    nb_max)
+                )
+            )
         except Exception:
-            print('Merci de saisir un nombre entre {} et {}'.format(nb_min, nb_max))
+            print('Merci de saisir un nombre entre {} et {}'.format(
+                nb_min,
+                nb_max)
+            )
         else:
             if nb_min <= user_number <= nb_max:
                 return user_number
 
 
-random_number = random.randint(MIN, MAX)
+nb_random = random.randint(NB_MIN_RANDOM, NB_MAX_RANDOM)
 nb_trial = 0
+nb_min_random = NB_MIN_RANDOM
+nb_max_random = NB_MAX_RANDOM
 while True:
     nb_trial += 1
-    user_input = ask_number(MIN, MAX)
-    if random_number == user_input:
+    print('Essai n°{} - '.format(nb_trial), end='')
+    nb_user_input = ask_number(nb_min_random, nb_max_random)
+    if nb_random == nb_user_input:
         print('Trouvé au bout de {} tentative(s)'.format(nb_trial))
         break
-    elif user_input > random_number:
-        print('Trop  grand')
+    elif nb_user_input > nb_random:
+        nb_max_random = nb_user_input
+        print('{} est trop grand'.format(nb_user_input))
     else:
-        print('Trop petit')
+        print('{} est trop petit'.format(nb_user_input))
+        nb_min_random = nb_user_input
 
     if nb_trial >= NB_ATTEMPTS:
-        print('Perdu, Le nombre aléatoire était {}'.format(random_number))
+        print('Perdu, Le nombre aléatoire était {}'.format(nb_random))
         break
