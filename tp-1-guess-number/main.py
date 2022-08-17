f1e775eade8cadc269aea62ec7014ad66110cb9e
techmindconsulting <techmind.consulting.sasu@gmail.com>
2022-08-17 20:52:07 +0200

    add use case tp-1

--- tp-1-guess-number/main.py
+++ tp-1-guess-number/main.py
@@ -5,10 +5,15 @@ NB_MIN_RANDOM = 1
 NB_MAX_RANDOM = 100
 NB_ATTEMPTS = 5
 
+nb_trial = 0
+
 
 def ask_number(nb_min, nb_max):
     while True:
         try:
+            global nb_trial
+            nb_trial += 1
+            print('Essai n°{} - '.format(nb_trial), end='')
             user_number = int(
                 input('Saisir un nombre un nombre entre {} et {} : '.format(
                     nb_min,
@@ -20,18 +25,18 @@ def ask_number(nb_min, nb_max):
                 nb_min,
                 nb_max)
             )
+            if nb_trial >= NB_ATTEMPTS:
+                return 0
         else:
             if nb_min <= user_number <= nb_max:
                 return user_number
 
 
 nb_random = random.randint(NB_MIN_RANDOM, NB_MAX_RANDOM)
-nb_trial = 0
 nb_min_random = NB_MIN_RANDOM
 nb_max_random = NB_MAX_RANDOM
-while True:
-    nb_trial += 1
-    print('Essai n°{} - '.format(nb_trial), end='')
+
+while nb_trial < NB_ATTEMPTS:
     nb_user_input = ask_number(nb_min_random, nb_max_random)
     if nb_random == nb_user_input:
         print('Trouvé au bout de {} tentative(s)'.format(nb_trial))
