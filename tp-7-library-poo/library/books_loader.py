866fcba7ecef9cfb99f36e6590a5e65845b532b4
techmindconsulting <70139511+techmindconsulting@users.noreply.github.com>
2022-08-24 14:25:17 +0200

    Update books_loader.py

--- tp-7-library-poo/library/books_loader.py
+++ tp-7-library-poo/library/books_loader.py
@@ -1,1 +1,11 @@
 
+import library.data as data
+from library.film import Film
+from library.friends_loader import FriendsLoader
+
+
+class BooksLoader:
+    def load(self):
+        books = []
+        # A completer: Charger à partier d'un fichier CSV ou JSON
+        return books
