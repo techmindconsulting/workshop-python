19b2defafe4ddac55d4fc91babfaf763fa9bd6f4
techmindconsulting <70139511+techmindconsulting@users.noreply.github.com>
2022-08-24 14:31:00 +0200

    Update books_manager.py

--- tp-7-library-poo/library/books_manager.py
+++ tp-7-library-poo/library/books_manager.py
@@ -1,1 +1,10 @@
+class BooksManager():
+    def __init__(self, books_loader):
+        if isinstance(books_loader, BooksLoader):
+            self.books = books_loader.load()
 
+    def add_film(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+
+# A completer
